1c15b89e1739f95bc45f90a14a2b1d50dd624dfc
RONAK-JAYSUKH-SAWANI <96838705+Ronaksawani@users.noreply.github.com>
2024-02-14 17:39:13 +0530

    chart updated

--- src/components/trading-view-home.jsx
+++ src/components/trading-view-home.jsx
@@ -1,67 +1,42 @@
 // TradingViewWidget.jsx
+import React, { useEffect, useRef, memo } from 'react';
 
-import React, { useEffect, useRef } from "react";
+function TradingViewWidget() {
+  const container = useRef();
 
-let tvScriptLoadingPromise;
-
-export default function TradingViewWidget() {
-  const onLoadScriptRef = useRef();
-
-  useEffect(() => {
-    onLoadScriptRef.current = createWidget;
-
-    if (!tvScriptLoadingPromise) {
-      tvScriptLoadingPromise = new Promise((resolve) => {
-        const script = document.createElement("script");
-        script.id = "tradingview-widget-loading-script";
-        script.src = "https://s3.tradingview.com/tv.js";
-        script.type = "text/javascript";
-        script.onload = resolve;
-
-        document.head.appendChild(script);
-      });
-    }
-
-    tvScriptLoadingPromise.then(
-      () => onLoadScriptRef.current && onLoadScriptRef.current()
-    );
-
-    return () => (onLoadScriptRef.current = null);
-
-    function createWidget() {
-      if (
-        document.getElementById("tradingview_81c45") &&
-        "TradingView" in window
-      ) {
-        new window.TradingView.widget({
-          autosize: true,
-          symbol: "BSE:TCS",
-          interval: "1",
-          timezone: "Asia/Kolkata",
-          theme: "light",
-          style: "1",
-          locale: "in",
-          enable_publishing: false,
-          withdateranges: true,
-          hide_side_toolbar: false,
-          allow_symbol_change: true,
-          save_image: false,
-          hide_volume: true,
-          container_id: "tradingview_81c45",
-        });
-      }
-    }
-  }, []);
+  useEffect(
+    () => {
+      const script = document.createElement("script");
+      script.src = "https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js";
+      script.type = "text/javascript";
+      script.async = true;
+      script.innerHTML = `
+        {
+          "autosize": true,
+          "symbol": "NASDAQ:AAPL",
+          "interval": "D",
+          "timezone": "Etc/UTC",
+          "theme": "light",
+          "style": "1",
+          "locale": "in",
+          "enable_publishing": false,
+          "hide_top_toolbar": true,
+          "save_image": false,
+          "calendar": false,
+          "hide_volume": true,
+          "support_host": "https://www.tradingview.com"
+        }`;
+      container.current.appendChild(script);
+    },
+    []
+  );
 
   return (
-    <div
-      className="tradingview-widget-container"
-      style={{ height: "100%", width: "100%" }}
-    >
-      <div
-        id="tradingview_81c45"
-        style={{ height: "calc(100% - 32px)", width: "100%" }}
-      />
+    <div className="tradingview-widget-container" ref={container} style={{ height: "100%", width: "100%" }}>
+      <div className="tradingview-widget-container__widget" style={{ height: "calc(100% - 32px)", width: "100%" }}></div>
+      <div className="tradingview-widget-copyright"><a href="https://in.tradingview.com/" rel="noopener nofollow" target="_blank"><span className="blue-text">Track all markets on TradingView</span></a></div>
     </div>
   );
 }
+
+export default memo(TradingViewWidget);
